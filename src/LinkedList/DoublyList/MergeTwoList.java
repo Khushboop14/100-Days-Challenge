51673b26afda6d3ef1d71bbca981267d3696413a
khushboop14 <p.khushbooparmar@gmail.com>
2024-08-07 01:14:40 +0530

    21day

---
+++ src/LinkedList/DoublyList/MergeTwoList.java
@@ -0,0 +1,4 @@
+package LinkedList.DoublyList;
+
+public class MergeTwoList {
+}
---
+++ src/LinkedList/SingleList/Node.java
@@ -0,0 +1,16 @@
+package LinkedList.SingleList;
+
+public class Node{
+    int data;
+    Node next;
+
+    Node(int data1, Node next1){
+        this.data = data;
+        this.next = next1;
+    }
+    Node(int data1){
+        this.data = data1;
+        this.next = null;
+    }
+
+}
